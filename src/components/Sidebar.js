37eec6803d49c338b2fc05c4003192ef8f7ad85b
viktoriialeseiko <vikyshaaa1@gmail.com>
2021-08-11 12:52:47 -0500

    sidebar avatar added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+//import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import './sidebar.css';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
-            
+                <Avatar />
+                <div className= 'sidebar__headerRight'>
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
             </div>
 
             <div className="sidebar__search">
