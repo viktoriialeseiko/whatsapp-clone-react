712ae46fe8a5383c616004dafc02e9c2917531ad
viktoriialeseiko <vikyshaaa1@gmail.com>
2021-08-11 12:30:08 -0500

    styling changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import React from 'react';
+import Sidebar from './components/Sidebar';
+import Chat from './components/Chat';
+import Login from './components/Login';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {useStateValue} from './StateProvider';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className="app_body">
+        {/* <Sidebar/>
+        <Chat/> */}
+            {/* <Router>
+              <Sidebar/>
+              <Switch>
+                <Route path="/rooms/:roomId">
+                  <Chat/>
+                </Route>
+                <Route path="/">
+                  <Chat/>
+                </Route>              
+              </Switch>            
+            </Router> */}
+          </div>
     </div>
   );
 }
