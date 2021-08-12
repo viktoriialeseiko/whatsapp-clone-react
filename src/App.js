81bd9f021ae57e383f9b53a7ec9504479a208720
viktoriialeseiko <vikyshaaa1@gmail.com>
2021-08-12 00:30:21 -0500

    login added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
-// import Login from './components/Login';
+import Login from './components/Login';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 // import {useStateValue} from './StateProvider';
 import './App.css';
 
 function App() {
+  //get the dispatch
+  // const [{user}, dispatch] = useStateValue();
+  const [user, setUser] = useState(null);
   return (
     <div className="app">
-      <div className="app_body">
-        <Router>
-          <Sidebar/>
-          <Switch>
-            <Route path="/rooms/:roomId">
-              <Chat/>
-            </Route>
-            <Route path="/">
-              <Chat/>
-            </Route>              
-          </Switch>            
-        </Router>
-      </div>
+        {!user ? (
+          <Login/>
+        ):(
+          <div className="app_body">
+            <Router>
+              <Sidebar/>
+              <Switch>
+                <Route path="/rooms/:roomId">
+                  <Chat/>
+                </Route>
+                <Route path="/">
+                  <Chat/>
+                </Route>              
+              </Switch>            
+            </Router>
+          </div>
+        )}
     </div>
   );
 }
